41aa27c0e03cc08d9a5767583909c07bf0f17a2c
Solo244 <solo244@gmail.com>
2016-05-09 16:39:03 +0200

    Lambda function js

--- dev/js/template/main.js
+++ dev/js/template/main.js
@@ -1,3 +1,24 @@
-$(function(){
-    console.log("ready!");
-});
+(function ($) {
+
+    /*
+     * Vars
+     */
+    var $body = $("html, body");
+
+    /*
+     * OMG, it's a function
+     */
+    function started(e){
+      var $this = $(this);
+      console.log("We've got lift off!");
+    }
+
+    /*
+     * Let's get ready to rumble
+     */
+    $(document).ready(function(){
+      // instantiate
+      $body.on("click", started);
+    });
+
+})(jQuery);
